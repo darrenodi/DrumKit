9d26ba89441b56a884cffca48d864fb5cc1ca85b
darrenodi <tantheta85@gmail.com>
2022-04-02 14:54:25 +0100

    Add animation to button press

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (var i =0; i<nod; i++){
     
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
     
   });
 }
@@ -54,4 +55,12 @@ function makeSound(key){
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+  activeButton.classList.remove("pressed");
+}, 100);
 }
